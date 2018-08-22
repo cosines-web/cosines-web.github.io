006d0885657ea8a82f3f5d30fc2d50e35d56455b
Jon Cockayne <benorn@gmail.com>
2018-08-22 14:57:23 +0100

    Restyled things to reduce the impact of a bug

--- scripts/expand_collapse.js
+++ scripts/expand_collapse.js
@@ -3,11 +3,15 @@ $(function() {
 	$('a[data-research-toggle]').click(function() {
 		var target = $(this).data('target');
 		var shown = $('.research-collapse.show');
+
 		if(shown.attr('id') == target) return;
 		if(shown.length == 0) {
 			$('#' + target).collapse('show');
 			return;
 		}
+
+		var collapsing = $('.research-collapse.collapsing');
+		if(collapsing.length != 0) return;
 
 		shown.collapse('hide');
 		shown.one('hidden.bs.collapse', function() {

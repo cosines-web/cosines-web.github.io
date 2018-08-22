fb6d3ba6061706a831f1805639bcb819a1737332
Jon Cockayne <benorn@gmail.com>
2018-08-22 15:34:06 +0100

    Moved people into their own folder, restored smooth scrolling.

--- scripts/nav.js
+++ scripts/nav.js
@@ -6,10 +6,14 @@ $(function() {
   var nav = $('#navbar');
   var nav_height = nav.height();
 
-  $(document).on('click', 'a[href^="#"]', function (event) {
+  $('a[href*="#"]:not(a[href="#"])').click(function (event) {
+
+      var is_this_page = (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname)
+      if(!is_this_page) return;
+
       event.preventDefault();
       
-      var scroll_to = $($(this).attr('href')).offset().top - nav_height - 20;
+      var scroll_to = $(this.hash).offset().top;
       if(scroll_to < 0)
         scroll_to = 0;
 
